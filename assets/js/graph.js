38d5d2d7a5bcb4904ffe94d3b38cb6c2e316805b
Tomek <tomek.regulski@gmail.com>
2021-03-08 14:31:44 -0500

    graph test working

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,41 +1,35 @@
-var wins = [
-    [ [2006,13],[2007,11],[2008,15],[2009,15],
-      [2010,18],[2011,21],[2012,28]
-    ]
+var wins = [[[0,13],[1,11],[2,15],[3,15],[4,18],[5,21],[6,28]]];
+var wins2 = [[[0,28]],[[1,28]],[[2,21]],[[3,20]],[[4,19]]];
+var teams = [
+    [0, "MCI"],
+    [1, "MUN"],
+    [2, "ARS"],
+    [3, "TOT"],
+    [4, "NEW"]
 ];
 
 window.onload = function () {
     Flotr.draw(
-        document.getElementById("chart"),
-        wins,
-        {
+        document.getElementById("chart"), wins2, {
+            title: "Premier League Wins (2011-2012)",
+            colors: ["#89AFD2", "#1D1D1D", "#DF021D", "#0E204B", "#E67840"],
             bars: {
-                show: true
+                show: true,
+                barWidth: 0.5,
+                shadowSize: 0,
+                fillOpacity: 1,
+                lineWidth: 0
+            },
+            yaxis: {
+                min: 0,
+                tickDecimals: 0
+            },
+            xaxis: {
+                ticks: teams
+            },
+            grid: {
+                horizontalLines: false,
+                verticalLines: false
             }
-        }
-    );
+    })
 };
-
-Flotr.draw(document.getElementById("chart"), wins, {
-    bars: {
-        show: true
-    },
-    yaxis: {
-        min: 0,
-        tickDecimals: 0
-    }
-});
-
-Flotr.draw(document.getElementById("chart"), wins, {
-    bars: {
-        show: true,
-        barWidth: 0.5
-    },
-    yaxis: {
-        min: 0,
-        tickDecimals: 0
-    },
-    xaxis: {
-        ticks: years
-    }
-});
